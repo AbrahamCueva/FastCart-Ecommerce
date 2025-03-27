a14a626cac60ee605246259f676f930761d22842
Abraham <ricoabraham879@gmail.com>
2025-03-27 14:58:55 -0500

    Manejo de pedidos de proveedores

--- vendor/views.py
+++ vendor/views.py
@@ -49,14 +49,14 @@ def products(request):
 
 @login_required
 def orders(request):
-    orders_list = store_models.Order.objects.filter(vendor=request.user, payment_status="Paid")
-    orders = paginate_queryset(request, orders, 2)
+    orders_list = store_models.Order.objects.filter(vendors=request.user, payment_status="Paid")
+    orders = paginate_queryset(request, orders_list, 10)
     context = {
         "orders_list": orders_list,
         "orders": orders,
     }
     return render(request, "vendor/orders.html", context)
-    
+
 @login_required
 def order_detail(request, order_id):
     order = store_models.Order.objects.get(vendors=request.user, order_id=order_id, payment_status="Paid")
@@ -77,7 +77,7 @@ def order_item_detail(request, order_id, item_id):
 
 @login_required
 def update_order_status(request, order_id):
-    order = store_models.Order.objects.get(vendor=request.user, order_id=order_id, payment_status="Paid")
+    order = store_models.Order.objects.get(vendors=request.user, order_id=order_id, payment_status="Paid")
     if request.method == "POST":
         order_status = request.POST.get("order_status")
         order.order_status = order_status
@@ -85,4 +85,24 @@ def update_order_status(request, order_id):
         
         messages.success(request, "Estado de pedido actualizado")
         return redirect("vendor:order_detail", order.order_id)
-  
+    return redirect("vendor:order_detail", order.order_id)
+
+@login_required
+def update_order_item_status(request, order_id, item_id):
+    order = store_models.Order.objects.get(vendors=request.user, order_id=order_id, payment_status="Paid")
+    item = store_models.OrderItem.objects.get(item_id=item_id, order=order)
+    if request.method == "POST":
+        order_status = request.POST.get("order_status")
+        shipping_service = request.POST.get("shipping_service")
+        tracking_id = request.POST.get("tracking_id")
+        
+        item.order_status = order_status
+        item.shipping_service = shipping_service
+        item.tracking_id = tracking_id
+        item.save()
+        
+        messages.success(request, "Estado producto en el pedido actualizado")
+        return redirect("vendor:order_item_detail", order.order_id, item.item_id)
+    return redirect("vendor:order_item_detail", order.order_id, item.item_id)
+
+
